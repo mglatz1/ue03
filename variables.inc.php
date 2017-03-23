5f2828d24071ceca625d73dbd9acf6f0c4eec48b
mglatz1 <mg.glatz@gmx.at>
2017-03-23 17:55:00 +0100

    Übungsstunde

---
+++ variables.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @var string FIRST_NAME Form field constant that defines how the form field for holding a first name is called
+ * (id/name).
+ */
+define("NUMBER", "number");
+
+define("MIN", 1);
+
+define("MAX", 100);
+
+define("FILE", "high-score.txt");
+
+define("LOWER_LIMIT", "lower_limit");
+
+define("UPPER_LIMIT", "upper_limit");
+
+define("GUESSES", "guesses");
+
+define("HIGHSCORE", "highscore");
